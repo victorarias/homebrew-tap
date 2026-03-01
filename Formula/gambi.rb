5d455ce4190e2ff3f81996b5a40a67872483af8b
Victor Arias <arias.sp@gmail.com>
2026-03-01 18:44:33 +0100

    gambi v0.1.2

--- Formula/gambi.rb
+++ Formula/gambi.rb
@@ -1,26 +1,26 @@
 class Gambi < Formula
   desc "Local MCP aggregator with execute-first workflow and OAuth refresh"
   homepage "https://github.com/victorarias/gambi"
-  version "0.1.0"
+  version "0.1.2"
   license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/victorarias/gambi/releases/download/v0.1.0/gambi-aarch64-apple-darwin.tar.gz"
-      sha256 "5c1d7703274cc8797ae83a55809be59429d422369c32f2988491d767a226826a"
+      url "https://github.com/victorarias/gambi/releases/download/v0.1.2/gambi-aarch64-apple-darwin.tar.gz"
+      sha256 "6d97c6ae7b2de3caa619df7e7c4dba17b36801d1a5840fb2677170bd1b1861c0"
     else
-      url "https://github.com/victorarias/gambi/releases/download/v0.1.0/gambi-x86_64-apple-darwin.tar.gz"
-      sha256 "e0400f499063b00fc73f3360ba1d409f4b3caa3600064510b33587fcae10c2fa"
+      url "https://github.com/victorarias/gambi/releases/download/v0.1.2/gambi-x86_64-apple-darwin.tar.gz"
+      sha256 "7c90744d2960055234ed008bdb4b8f1470ac5dba553809194c8946886912fa32"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/victorarias/gambi/releases/download/v0.1.0/gambi-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "b9d191a4db07d57ad886c3fdda98a55716ab9b10739c4475c4ab384bc35642ca"
+      url "https://github.com/victorarias/gambi/releases/download/v0.1.2/gambi-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "e63b111fa91b8f3c97e8d44634ec040647946d425bac31c012e6e1c18072dd26"
     else
-      url "https://github.com/victorarias/gambi/releases/download/v0.1.0/gambi-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "526b8a5937cd59fa3b210db84f2062d0c1b634c757b02b0ef6ca15018d7960e9"
+      url "https://github.com/victorarias/gambi/releases/download/v0.1.2/gambi-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "cce8bb1b8df791274bc4e8583e30b507c82e8c428bbf122a7f9360869f78937e"
     end
   end
 
